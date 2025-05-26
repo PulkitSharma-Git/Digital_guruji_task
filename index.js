c0c406f68b1debd73bc92cb08deb05d8fca5c810
Pulkit Sharma <Pulkit2005sharma@gmail.com>
2025-05-26 22:28:45 +0530

    deploy4

--- index.js
+++ index.js
@@ -27,20 +27,22 @@ app.get('/screenshot', async (req, res) => {
   const scale = parseFloat(deviceScaleFactor) || 2;
 
   try {
+    console.log('Chromium path:', puppeteer.executablePath());
+
     const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: puppeteer.executablePath(),
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-gpu'
-    ]
-  });
+  headless: true,
+  executablePath: puppeteer.executablePath(),
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu'
+  ]
+});
 
 
     const page = await browser.newPage();
