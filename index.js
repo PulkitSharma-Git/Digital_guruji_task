ee9e96e5b66ce590a7a50e4e6a851071c24d92e7
Pulkit Sharma <Pulkit2005sharma@gmail.com>
2025-05-26 21:38:58 +0530

    deploy2

--- index.js
+++ index.js
@@ -29,7 +29,16 @@ app.get('/screenshot', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu'
+      ]
     });
 
     const page = await browser.newPage();
